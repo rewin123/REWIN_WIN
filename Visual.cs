4f25ec0bda849095589d5db2210eaf5fb2c92ce1
rewin <rewin1996@gmail.com>
2017-11-07 15:32:28 +0300

    Finak 2

---
+++ Visual.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk.Model;
+
+namespace Com.CodeGame.CodeWars2017.DevKit.CSharpCgdk
+{
+    public partial class Visual : Form
+    {
+        Dictionary<TerrainType, SolidBrush> terrain_colors = new Dictionary<TerrainType, SolidBrush>();
+        public Visual()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw(World world, Game game)
+        {
+            
+        }
+    }
+}
